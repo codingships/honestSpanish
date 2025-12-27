0b8a65128bb0160496b77e4f588493c297103afe
codingships <213037512+codingships@users.noreply.github.com>
2025-12-27 16:37:55 +0100

    limpieza del repositorio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
-// import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,16 +13,16 @@ export default defineConfig({
         tailwind({
             applyBaseStyles: false,
         }),
-        // sitemap({
-        //   i18n: {
-        //     defaultLocale: 'es',
-        //     locales: {
-        //       es: 'es-ES',
-        //       en: 'en-US',
-        //       ru: 'ru-RU',
-        //     },
-        //   },
-        // }),
+        sitemap({
+            i18n: {
+                defaultLocale: 'es',
+                locales: {
+                    es: 'es-ES',
+                    en: 'en-US',
+                    ru: 'ru-RU',
+                },
+            },
+        }),
     ],
     adapter: netlify(),
     i18n: {
